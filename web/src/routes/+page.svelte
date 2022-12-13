fdfe7fe4c1af83f9ac0be1addf3757aab3dad317
Montefusco <pedro.montefusco@outlook.com.br>
2022-12-13 07:03:54 -0300

    first commit

---
+++ web/src/routes/+page.svelte
@@ -0,0 +1,80 @@
+<div class="grid h-screen place-items-center">
+
+  <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+  <div tabindex="0" class="collapse collapse-plus border border-base-300 bg-base-100 rounded-box">
+    <div class="collapse-title text-xl font-medium">
+      Pedro Montefusco
+    </div>
+    <div class="collapse-content"> 
+      <div class="stats">
+  
+        <div class="stat">
+          <div class="stat-title">Total Page Views</div>
+          <div class="stat-value">89,400</div>
+          <div class="stat-desc">21% more than last month</div>
+        </div>
+        <div class="stat">
+          <div class="stat-title">Total Page Views</div>
+          <div class="stat-value">89,400</div>
+          <div class="stat-desc">21% more than last month</div>
+        </div>
+        <div class="stat">
+          <div class="stat-title">Total Page Views</div>
+          <div class="stat-value">89,400</div>
+          <div class="stat-desc">21% more than last month</div>
+        </div>
+        <div class="stat">
+          <div class="stat-title">Total Page Views</div>
+          <div class="stat-value">89,400</div>
+          <div class="stat-desc">21% more than last month</div>
+        </div>
+        <div class="stat">
+          <div class="stat-title">Total Page Views</div>
+          <div class="stat-value">89,400</div>
+          <div class="stat-desc">21% more than last month</div>
+        </div>
+        
+      </div>
+    </div>
+  </div>
+
+  <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+  <div tabindex="0" class="collapse collapse-plus border border-base-300 bg-base-100 rounded-box">
+    <div class="collapse-title text-xl font-medium">
+      Pedro Montefusco
+    </div>
+    <div class="collapse-content"> 
+      <div class="stats-vertical">
+  
+        <div class="stat">
+          <div class="stat-title">Total dasdasPage Views</div>
+          <div class="stat-value">89,400</div>
+          <div class="stat-value">20,400</div>
+          <div class="stat-desc">21% more than last month</div>
+        </div>
+        <div class="stat">
+          <div class="stat-title">Total Page Views</div>
+          <div class="stat-value">89,400</div>
+          <div class="stat-desc">21% more than last month</div>
+        </div>
+        <div class="stat">
+          <div class="stat-title">Total Page Views</div>
+          <div class="stat-value">89,400</div>
+          <div class="stat-desc">21% more than last month</div>
+        </div>
+        <div class="stat">
+          <div class="stat-title">Total Page Views</div>
+          <div class="stat-value">89,400</div>
+          <div class="stat-desc">21% more than last month</div>
+        </div>
+        <div class="stat">
+          <div class="stat-title">Total Page Views</div>
+          <div class="stat-value">89,400</div>
+          <div class="stat-desc">21% more than last month</div>
+        </div>
+        
+      </div>
+    </div>
+  </div>
+
+</div>
