04fbad3af96f4350d1ea24ce48d83f0e99cba438
Nylphy <Pedro.Montefusco@outlook.com.br>
2022-12-14 22:05:41 -0300

    update

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,10 @@
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
-			edge: false,
-			split: true
 		})
 	},
 	preprocess: [
