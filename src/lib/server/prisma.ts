af59c4949b005c287551f8832a61fc20b28da17d
Montefusco <pedro.montefusco@outlook.com.br>
2023-01-25 18:51:41 -0300

    updated

--- src/lib/server/prisma.ts
+++ src/lib/server/prisma.ts
@@ -7,4 +7,3 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 export { prisma };
-export const prerender = true;
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,6 @@
 import type { PageServerLoad } from './$types';
 import { prisma } from '$lib/server/prisma';
 
-export const prerender = true;
 export const load: PageServerLoad = async () => {
 	return {
 		produtos: await prisma.produto.findMany()
