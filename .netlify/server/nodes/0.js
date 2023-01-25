c0fbdbf2dc4cc2752445c10eb8b7e0fb16a3b373
Nylphy <45796087+Nylphy@users.noreply.github.com>
2023-01-25 18:44:23 -0300

    Delete .netlify directory

--- .netlify/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-export const component = async () => (await import('../entries/pages/_layout.svelte.js')).default;
-export const file = '_app/immutable/components/pages/_layout.svelte-951a1eaf.js';
-export const imports = ["_app/immutable/components/pages/_layout.svelte-951a1eaf.js","_app/immutable/chunks/index-f26ceaca.js"];
-export const stylesheets = ["_app/immutable/assets/_layout-8b8b1232.css"];
-export const fonts = [];
